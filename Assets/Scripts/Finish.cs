804ee9d95efec7e15b5612e6f112129c65714fb7
kadeHennacy <kade.hennacy@gmail.com>
2022-05-04 12:58:37 -0400

    All scenes are connected via finish object that triggers scene change upon collision

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 // This loads the next scene when player reaches the end of a level
 // based on https://www.youtube.com/watch?v=dO5BzWYqEdY
@@ -14,15 +15,16 @@ public class Finish : MonoBehaviour
     {
         finishSound = GetComponent<AudioSource>();
     }
-
+    //player must have ridgidbody2d
     private void OnTriggerEnter2D(Collider2D col){
         if (col.gameObject.name.Contains("Player")){
-            finishSound.Play();
+            // finishSound.Play();
             CompleteLevel();
         }
     }
 
     private void CompleteLevel(){
-
+        Debug.Log("complete Level");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
